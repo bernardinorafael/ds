d173bb3612069eb90447e04610f98c286cdf7ab3
Rafael Bernardino <rafaelferreirab2@gmail.com>
2026-02-27 19:49:00 -0300

    refactor(fieldset): remove unused Fieldset components and adjust

--- src/components/fieldset/fieldset.stories.tsx
+++ src/components/fieldset/fieldset.stories.tsx
@@ -43,46 +43,10 @@ export const InsideCard: Story = {
         </Card.Row>
         <Card.Row>
           <Fieldset
-            legend="Este é seu nome de exibição"
-            description="Selecione um nome que você gostaria que outras pessoas vissem no seu perfil"
-          >
-            <div className="flex flex-col gap-2">
-              <p className="text-word-primary text-base font-medium">Nome</p>
-              <input
-                type="text"
-                defaultValue="rafael bernardino"
-                className="border-border rounded-md border bg-white px-3 py-2 text-sm"
-              />
-            </div>
-          </Fieldset>
-        </Card.Row>
-        <Card.Row>
-          <Fieldset
-            legend="Altere seu username"
-            description={[
-              "Sua próxima oportunidade de alterá-lo será:",
-              "sábado, 12 de abril de 2025 às 19:40",
-            ]}
-          >
-            <div className="flex flex-col gap-2">
-              <p className="text-word-primary text-base font-medium">Username</p>
-              <p className="text-word-secondary text-sm">
-                Podem conter apenas letras, números, sublinhados e pontos
-              </p>
-              <input
-                type="text"
-                defaultValue="bernardino"
-                className="border-border rounded-md border bg-white px-3 py-2 text-sm"
-              />
-            </div>
-          </Fieldset>
-        </Card.Row>
-        <Card.Row>
-          <Fieldset
             legend="Notificações por email"
             description="Escolha quais emails você gostaria de receber"
           >
-            <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-5">
               <CheckboxWithLabel id="product" defaultChecked>
                 Atualizações de produto
               </CheckboxWithLabel>
